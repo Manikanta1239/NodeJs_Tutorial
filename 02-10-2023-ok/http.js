32552c0de5afd1145d18edf0d979c4f88b856c83
Manikanta <134567883+Manikanta1239@users.noreply.github.com>
2025-04-11 13:27:55 +0530

    Update http.js

--- 02-10-2023-ok/http.js
+++ 02-10-2023-ok/http.js
@@ -1,9 +1,16 @@
+// Import the built-in 'http' module to create an HTTP server
 const http = require('http');
 
+// Create an HTTP server using the createServer method
+// This method takes a callback function that handles incoming requests (req) and sends responses (res)
 const server = http.createServer((req, res) => {
+    // End the response with a message: "Hello World"
     res.end('Hello World');
 });
 
-server.listen(5500,()=> {
-    console.log('the server is running....');
+// Start the server and make it listen on port 5500
+// Once the server starts, the callback function logs a message to the console
+server.listen(5500, () => {
+    console.log('The server is running....');
 });
+
