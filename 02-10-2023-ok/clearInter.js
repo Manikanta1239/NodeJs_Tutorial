d3f81eb8febb16fd871e426f6669dfd3aea38930
Manikanta <134567883+Manikanta1239@users.noreply.github.com>
2025-04-11 13:26:59 +0530

    Update clearInter.js

--- 02-10-2023-ok/clearInter.js
+++ 02-10-2023-ok/clearInter.js
@@ -1,11 +1,20 @@
+// Initialize a counter variable to keep track of how many times the interval has run
 let counter = 0;
-const intervalId = setInterval(
-    () => {console.log('Hello World');
+
+// Start a repeating timer using setInterval. This function will run every 1000 milliseconds (1 second)
+const intervalId = setInterval(() => {
+    // Log "Hello World" to the console each time the interval runs
+    console.log('Hello World');
+    
+    // Increment the counter by 2
     counter += 2;
 
-    if(counter === 5){
+    // Check if the counter has reached exactly 5
+    if (counter === 5) {
+        // If so, log "Done" to the console
         console.log('Done');
+        
+        // Stop the interval so it doesn't run anymore
         clearInterval(intervalId);
     }
-},1000);
-
+}, 1000); // 1000 milliseconds = 1 second
